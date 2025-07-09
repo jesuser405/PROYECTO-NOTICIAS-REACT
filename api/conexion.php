9a69f83bd2f425bef9502be7a0933760de373cff
jesus <jesuser405@hotmail.com>
2025-07-09 12:35:34 +0200

    qqqq

--- api/conexion.php
+++ api/conexion.php
@@ -1,11 +1,16 @@
 <?php
+// Configuración de la conexión a la base de datos
+$host = "localhost";
+$user = "jesus405"; // Usuario de la base de datos
+$password = "Jk123456$45"; // Contraseña de la base de datos
+$database = "cursonascor"; // Nombre de la base de datos
 
-// INI
-
-// Conexión
 try {
-   $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
-   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // Crear una nueva conexión PDO a MySQL con codificación utf8mb4
+  $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
+  // Configurar el modo de error para que lance excepciones
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-   die(" Error de conexión: " . $e->getMessage());
+  // Mostrar mensaje de error si la conexión falla
+  die(" Error de conexión: " . $e->getMessage());
 }
