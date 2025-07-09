f8a19c5f1c37db5d005960a5cc5d0408d611de7e
jesus <jesuser405@hotmail.com>
2025-07-09 12:33:01 +0200

    Proyecto clonado desde PROYECTO-NOTICIAS-REACT

--- api/conexion.php
+++ api/conexion.php
@@ -1,15 +1,11 @@
 <?php
 
 // INI
-$host = "localhost";
-$user = "jesus405"; // Usuario de la base de datos
-$password = "Jk123456$45"; // Contraseña de la base de datos
-$database = "cursonascor"; // Nombre de la base de datos
+
 // Conexión
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
-  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  // Mostrar mensaje de error si la conexión falla
-  die(" Error de conexión: " . $e->getMessage());
+   die(" Error de conexión: " . $e->getMessage());
 }
